e5683503d503d929ad5d587d45015d64ec2e4f50
capnchill <subodhchilwal123@gmail.com>
2024-05-01 14:20:13 +0530

    tailwind config added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+	Welcome to SvelteKit
+</h1>
