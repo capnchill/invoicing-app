41c135197d35be4490677849402ca6a31fb7cec8
capnchill <subodhchilwal123@gmail.com>
2024-05-06 00:34:02 +0530

    added additional conditional validation to the existing and new client

--- src/routes/(dashboard)/invoices/InvoiceForm.svelte
+++ src/routes/(dashboard)/invoices/InvoiceForm.svelte
@@ -49,7 +49,8 @@
 			<label for="client">Client</label>
 
 			<div class="flex items-end gap-x-5">
-				<select name="client" id="client" class="bg-transparent">
+				<select name="client" id="client" class="bg-transparent" required={!isNewClient}>
+					<option />
 					{#each $clients as client}
 						<option value={client.id}>{client.name}</option>
 					{/each}
@@ -67,7 +68,7 @@
 		{:else}
 			<label for="newClient">New Client</label>
 			<div class="flex items-end gap-x-5">
-				<input type="text" name="newClient" />
+				<input type="text" name="newClient" required={isNewClient} />
 				<div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
 				<Button
 					label="Existing Client"
@@ -92,7 +93,7 @@
 		<div class="field col-span-6 grid gap-x-5" transition:slide>
 			<div class="field col-span-6">
 				<label for="email">Client's Email</label>
-				<input type="email" name="email" id="email" />
+				<input type="email" name="email" id="email" required={isNewClient} />
 			</div>
 
 			<div class="field col-span-6">
