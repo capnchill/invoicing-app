4805692b187e3ca21e6bc290ccb399ff831d1293
capnchill <subodhchilwal123@gmail.com>
2024-05-13 02:15:33 +0530

    added animation to search component input using css, ::placeholder pseudo selectors, focus states and transform and transition

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -9,8 +9,28 @@
 	<input
 		type="text"
 		name="search"
-		placeholder="search by keyword"
-		class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
+		placeholder="Search"
+		class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent pr-[70px] font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] md:pr-0 lg:w-72 lg:text-xl"
 	/>
-	<button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Search</button>
+	<button
+		class="pointer-events-none absolute left-8 transform font-sansSerif text-lg font-black text-pastelPurple transition-transform lg:text-xl"
+	>
+		Search
+	</button>
 </div>
+
+<style lang="postcss">
+	::placeholder {
+		color: transparent;
+	}
+
+	input:not(:placeholder-shown) + button,
+	input:focus + button {
+		@apply pointer-events-auto left-auto right-0 translate-x-[calc(100%-65px)] text-right text-daisyBush md:left-0 md:right-auto md:translate-x-[16rem] lg:translate-x-80;
+	}
+
+	input + button:focus,
+	input:focus + button:hover {
+		@apply text-lavenderIndigo;
+	}
+</style>
