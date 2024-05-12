aa1c7cd0605c0a2aa6947c16f14a71edc7684376
capnchill <subodhchilwal123@gmail.com>
2024-05-12 19:31:13 +0530

    Added settings page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -44,7 +44,7 @@
 		<ul class="flex flex-col gap-2 text-2xl font-bold">
 			<li><a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a></li>
 			<li><a href="/clients" class:active={$page.url.pathname === '/clients'}>Clients</a></li>
-			<li><a href="/settings">Settings</a></li>
+			<li><a href="/settings" class:active={$page.url.pathname === '/settings'}>Settings</a></li>
 			<li><a href="/">Logout</a></li>
 		</ul>
 	</nav>
