83c3d70bb45cee52d5bb6d4bbbe95e66f61a016d
capnchill <subodhchilwal123@gmail.com>
2024-05-13 07:39:04 +0530

    swipe event listener added for specific screen breakpoint inside the swipe actions file

--- src/lib/actions/Swipe.ts
+++ src/lib/actions/Swipe.ts
@@ -1,72 +1,49 @@
+import type { Action } from 'svelte/action';
 import { spring } from 'svelte/motion';
 
-type SwipeProps = {
+interface SwipeProps {
 	triggerReset?: boolean;
-};
+}
 
 export function swipe(node: HTMLElement, params: SwipeProps) {
 	let x: number;
 	let startingX: number;
-	const elementWidth = node.clientWidth;
-	let triggerReset = params.triggerReset || false;
+	let elementWidth = node.clientWidth;
+	let triggerReset = params?.triggerReset || false;
 
-	const coordinates = spring({ x: 0, y: 0 }, { stiffness: 0.2, damping: 0.4 });
+	const coordinates = spring(
+		{ x: 0, y: 0 },
+		{
+			stiffness: 0.2,
+			damping: 0.4
+		}
+	);
 
 	coordinates.subscribe((coords) => {
 		node.style.transform = `translate3d(${coords.x}px, 0, 0)`;
 	});
 
-	node.addEventListener('mousedown', handleMouseDown);
-
-	function handleMouseDown(event: MouseEvent) {
-		startingX = event.clientX;
-		console.log(startingX);
-		x = event.clientX;
-		window.addEventListener('mousemove', handleMouseMove);
-		window.addEventListener('mouseup', handleMouseUp);
+	if (isMobileBreakpoint()) {
+		node.addEventListener('mousedown', handleMouseDown);
 	}
 
-	function handleMouseMove(event: MouseEvent) {
-		// delta x is the difference from where we clicked to where we are currently
-		const dx = event.clientX - x;
-		x = event.clientX;
-
-		coordinates.update((coords) => {
-			return { x: coords.x + dx, y: 0 };
-		});
-	}
-
-	function updateCoordindates(x: number) {
-		coordinates.update(() => {
-			return { x, y: 0 };
-		});
-	}
-
-	function moveCardOver(endingX: number) {
-		const leftSnapX = elementWidth * -0.95;
-		const rightSnapX = 0;
-
-		const movement = startingX - endingX;
-
-		if (movement > 20) {
-			// swiped left
-			x = leftSnapX;
-			outofview();
+	// listen for browser resize
+	window.addEventListener('resize', () => {
+		if (isMobileBreakpoint()) {
+			node.addEventListener('mousedown', handleMouseDown);
 		} else {
-			// swiped right
-			x = rightSnapX;
-			backInView();
+			node.removeEventListener('mousedown', handleMouseDown);
 		}
-		updateCoordindates(x);
-	}
 
-	function handleMouseUp(event: MouseEvent) {
-		const endingX = event.clientX;
+		// update the card width
+		elementWidth = node.clientWidth;
+	});
 
-		moveCardOver(endingX);
-
-		window.removeEventListener('mousemove', handleMouseMove);
-		window.removeEventListener('mouseup', handleMouseUp);
+	function isMobileBreakpoint() {
+		const mediaQuery = window.matchMedia('(max-width: 1024px)');
+		if (mediaQuery.matches) {
+			return true;
+		}
 	}
 
 	function resetCard() {
@@ -80,13 +57,56 @@ export function swipe(node: HTMLElement, params: SwipeProps) {
 		node.dispatchEvent(new CustomEvent('outofview'));
 	}
 
-	function backInView() {
-		node.dispatchEvent(new CustomEvent('backInView'));
+	function handleMouseDown(event: MouseEvent) {
+		x = event.clientX;
+		startingX = event.clientX;
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseup', handleMouseUp);
+	}
+
+	function handleMouseMove(event: MouseEvent) {
+		// Delta x = difference from where we clicked vs where we are currently.
+		const dx = event.clientX - x;
+		x = event.clientX;
+		coordinates.update(($coords) => {
+			return {
+				x: $coords.x + dx,
+				y: 0
+			};
+		});
+	}
+
+	function updateCoordinates(x: number) {
+		coordinates.update(() => {
+			return { x, y: 0 };
+		});
+	}
+
+	function moveCardOver(endingX: number) {
+		const leftSnapX = elementWidth * -0.95;
+		const rightSnapX = 0;
+		const movement = startingX - endingX;
+
+		// swiped left
+		if (movement > 20) {
+			x = leftSnapX;
+			outofview();
+		} // swiped right
+		else {
+			x = rightSnapX;
+		}
+		updateCoordinates(x);
+	}
+
+	function handleMouseUp(event: MouseEvent) {
+		const endingX = event.clientX;
+		moveCardOver(endingX);
+		window.removeEventListener('mousemove', handleMouseMove);
+		window.removeEventListener('mouseup', handleMouseUp);
 	}
 
 	return {
 		update(newParams: SwipeProps) {
-			// the code inside update runs whenever the parameters update or change
 			if (newParams.triggerReset) {
 				resetCard();
 			}
